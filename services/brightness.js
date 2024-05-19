a01f8152b7691ae26fba4dbabed0b5c60f7dc9ce
Noah Bauer <bauer.noah712@gmx.de>
2024-05-19 17:48:45 +0200

    fix monitor brightness

--- services/brightness.js
+++ services/brightness.js
@@ -67,8 +67,11 @@ class BrightnessDdcService extends BrightnessServiceBase {
     Service.register(this);
   }
 
-  constructor(busNum) {
+  monitorName = "";
+
+  constructor(busNum, monitorName) {
     super();
+    this.monitorName = monitorName;
     this._busNum = busNum;
     Utils.execAsync(`ddcutil -b ${this._busNum} getvcp 10 --brief`)
       .then((out) => {
@@ -98,7 +101,7 @@ async function listDdcMonitorsSnBus() {
       const reg = /^Display \d+/;
       if (!reg.test(display)) return;
       const lines = display.split("\n");
-      const sn = lines[3].split(":")[3];
+      const sn = lines[2].split("card2-")[1];
       const busNum = lines[1].split("/dev/i2c-")[1];
       ddcSnBus[sn] = busNum;
     });
@@ -114,7 +117,7 @@ const service = Array(numMonitors);
 const ddcSnBus = await listDdcMonitorsSnBus();
 for (let i = 0; i < service.length; i++) {
   const monitorName = Hyprland.monitors[i].name;
-  const monitorSn = Hyprland.monitors[i].serial;
+  // const monitorSn = Hyprland.monitors[i].serial;
   const preferredController =
     userOptions.brightness.controllers[monitorName] ||
     userOptions.brightness.controllers.default ||
@@ -125,11 +128,15 @@ for (let i = 0; i < service.length; i++) {
         service[i] = new BrightnessCtlService();
         break;
       case "ddcutil":
-        service[i] = new BrightnessDdcService(ddcSnBus[monitorSn]);
+        service[i] = new BrightnessDdcService(
+          ddcSnBus[monitorName],
+          monitorName,
+        );
         break;
       case "auto":
-        if (monitorSn in ddcSnBus)
-          service[i] = new BrightnessDdcService(ddcSnBus[monitorSn]);
+        // if (monitorSn in ddcSnBus)
+        if (monitorName in ddcSnBus)
+          service[i] = new BrightnessDdcService(ddcSnBus[monitorName]);
         else service[i] = new BrightnessCtlService();
         break;
       default:
@@ -138,8 +145,38 @@ for (let i = 0; i < service.length; i++) {
   }
 }
 
+class BrightnessCommandLineService {
+  brightnessServices;
+
+  constructor(brightnessServices) {
+    this.brightnessServices = brightnessServices;
+  }
+
+  increaseBy(value) {
+    this.getServiceForMonitor().then(
+      (service) => (service.screen_value += value),
+    );
+  }
+
+  decreaseBy(value) {
+    this.getServiceForMonitor().then(
+      (service) => (service.screen_value -= value),
+    );
+  }
+
+  async getServiceForMonitor() {
+    let activeMonitorId = JSON.parse(
+      await Utils.execAsync(`hyprctl activeworkspace -j`),
+    ).monitorID;
+    return this.brightnessServices.at(activeMonitorId);
+  }
+}
+
+let test = new BrightnessCommandLineService(service);
+test.screen_value += 0.05;
+
 // make it global for easy use with cli
-globalThis.brightness = service[0];
+globalThis.brightness = new BrightnessCommandLineService(service);
 
 // export to use in other modules
 export default service;
