b56fae10b8e44749f631a3eb1947351f7dba04d0
Noah Bauer <accounts@noahbauer.de>
2024-06-20 10:50:31 +0200

    hide feels like temperature from bar

--- modules/bar/normal/system.js
+++ modules/bar/normal/system.js
@@ -166,7 +166,8 @@ const BatteryModule = () => Stack({
                             const feelsLike = weather.current_condition[0][`FeelsLike${userOptions.weather.preferredUnit}`];
                             const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
                             self.children[0].label = weatherSymbol;
-                            self.children[1].label = `${temperature}°${userOptions.weather.preferredUnit} • Feels like ${feelsLike}°${userOptions.weather.preferredUnit}`;
+                            const feelsLikeString = ` • Feels like ${feelsLike}°${userOptions.weather.preferredUnit}`;
+                            self.children[1].label = `${temperature}°${userOptions.weather.preferredUnit}`;
                             self.tooltipText = weatherDesc;
                         }).catch((err) => {
                             try { // Read from cache
@@ -179,7 +180,8 @@ const BatteryModule = () => Stack({
                                 const feelsLike = weather.current_condition[0][`FeelsLike${userOptions.weather.preferredUnit}`];
                                 const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
                                 self.children[0].label = weatherSymbol;
-                                self.children[1].label = `${temperature}°${userOptions.weather.preferredUnit} • Feels like ${feelsLike}°${userOptions.weather.preferredUnit}`;
+                                const feelsLikeString = ` • Feels like ${feelsLike}°${userOptions.weather.preferredUnit}`;
+                                self.children[1].label = `${temperature}°${userOptions.weather.preferredUnit}`;
                                 self.tooltipText = weatherDesc;
                             } catch (err) {
                                 print(err);
