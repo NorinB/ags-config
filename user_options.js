416ddcb66c24b9fc117d04d2a798c97922565419
Noah Bauer <accounts@noahbauer.de>
2024-06-23 03:01:48 +0200

    update ags config and add blueman-manager icon

--- user_options.js
+++ user_options.js
@@ -25,6 +25,60 @@ const userConfigOptions = {
             default: 'ddcutil',
         },
     },
+    cheatsheet: {
+        keybinds: {
+            configPath: '~/.config/hypr/configs/keybinds.conf', // Path to hyprland keybind config file. Leave empty for default (~/.config/hypr/hyprland/keybinds.conf)
+        },
+    },
+    dock: {
+        enabled: true,
+        hiddenThickness: 5,
+        pinnedApps: ['dolphin', 'spotify', 'brave'],
+        layer: 'top',
+        monitorExclusivity: true, // Dock will move to other monitor along with focus if enabled
+        searchPinnedAppIcons: false, // Try to search for the correct icon if the app class isn't an icon name
+        trigger: [], // client_added, client_move, workspace_active, client_active, client_removed
+        // Automatically hide dock after `interval` ms since trigger
+        autoHide: [
+            // {
+            //     trigger: 'client-added',
+            //     interval: 500,
+            // },
+            // {
+            //     trigger: 'client-removed',
+            //     interval: 500,
+            // },
+        ],
+    },
+    icons: {
+        // Find the window's icon by its class with levenshteinDistance
+        // The file names are processed at startup, so if there
+        // are too many files in the search path it'll affect performance
+        // Example: ['/usr/share/icons/Tela-nord/scalable/apps']
+        searchPaths: [''],
+        symbolicIconTheme: {
+            dark: 'Adwaita',
+            light: 'Adwaita',
+        },
+        substitutions: {
+            'blueman-manager': 'bluetooth',
+            'code-url-handler': 'visual-studio-code',
+            Code: 'visual-studio-code',
+            'GitHub Desktop': 'github-desktop',
+            'Minecraft* 1.20.1': 'minecraft',
+            'gnome-tweaks': 'org.gnome.tweaks',
+            'pavucontrol-qt': 'pavucontrol',
+            wps: 'wps-office2019-kprometheus',
+            wpsoffice: 'wps-office2019-kprometheus',
+            '': 'image-missing',
+        },
+        regexSubstitutions: [
+            {
+                regex: /^steam_app_(\d+)$/,
+                replace: 'steam_icon_$1',
+            },
+        ],
+    },
     music: {
         preferredPlayer: 'spotify',
     },
@@ -51,26 +105,6 @@ const userConfigOptions = {
         dateFormatLong: '%a, %d. %B', // On bar
         dateInterval: 5000,
         dateFormat: '%d. %B', // On notif time
-    },
-    dock: {
-        enabled: true,
-        hiddenThickness: 5,
-        pinnedApps: ['dolphin', 'spotify', 'brave'],
-        layer: 'top',
-        monitorExclusivity: true, // Dock will move to other monitor along with focus if enabled
-        searchPinnedAppIcons: false, // Try to search for the correct icon if the app class isn't an icon name
-        trigger: [], // client_added, client_move, workspace_active, client_active, client_removed
-        // Automatically hide dock after `interval` ms since trigger
-        autoHide: [
-            // {
-            //     trigger: 'client-added',
-            //     interval: 500,
-            // },
-            // {
-            //     trigger: 'client-removed',
-            //     interval: 500,
-            // },
-        ],
     },
 };
 
